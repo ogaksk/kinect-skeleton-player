2b75fd9bb11955cf7f19f4f8a1172aca4d8ea936
ogaksk <ogaksk@ogawakeisuke-no-MacBook-Pro.local>
2016-11-11 17:54:34 +0900

    メソッド、描画形式を修正

--- app.js
+++ app.js
@@ -22,28 +22,56 @@ var data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 var startTime = data[0].timestamp;
 
 
-io.sockets.on("connection", function () {
-  console.log("aa");
-  io.sockets.emit('floorclipplane', data[0].floorClipPlane);
+// io.sockets.on("connection", function () {
+//   io.sockets.emit('floorclipplane', data[0].floorClipPlane);
+// });
+
+var quesOfms = [];
+var que = [];
+Object.keys(data).forEach(function (key) {
+  quesOfms.push(data[key].timestamp);
 });
 
+for (var i = 0; i < quesOfms.length -1; i ++) {
+  que.push(quesOfms[i+1] - quesOfms[i])
+}
 
 
-setInterval( function() {
-  if (data[nextFrame.index]) {
-    nextFrame.sec = data[nextFrame.index].timestamp - startTime;
-    if (nextFrame.sec == frame) {
-      io.sockets.emit('bodyFrame', data[nextFrame.index]);
-      nextFrame.index += 1; 
-    } else if (data[nextFrame.index].timestamp == data[nextFrame.index - 1].timestamp) {
-      nextFrame.index += 1; 
+var queIndex = 0;
+function queing() {
+
+  setTimeout(function () {
+    io.sockets.emit('bodyFrame', data[queIndex]);
+
+    if (queIndex  ==  que.length) {
+      queIndex = 0;
+      
+    } else {
+      queIndex += 1;
     }
-    frame ++ ;
-  } else {
-    frame = 0;
-    nextFrame = {index: 0, sec: 0};
-  }
-}, 1) 
+
+    queing();
+  }, que[queIndex]);  
+}
+
+queing()
+
+// setInterval( function() {
+//   if (data[nextFrame.index]) {
+
+//     nextFrame.sec = data[nextFrame.index].timestamp - startTime;
+//     if (nextFrame.sec == frame) {
+//       io.sockets.emit('bodyFrame', data[nextFrame.index]);
+//       nextFrame.index += 1; 
+//     } else if (data[nextFrame.index].timestamp == data[nextFrame.index - 1].timestamp) {
+//       nextFrame.index += 1; 
+//     }
+//     frame ++ ;
+//   } else {
+//     frame = 0;
+//     nextFrame = {index: 0, sec: 0};
+//   }
+// }, 1) 
 
 
 
